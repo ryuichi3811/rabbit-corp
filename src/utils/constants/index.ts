3716c66d153975de87006f5f23b9fd273086dd21
ryuichi <ryuichi19633811@gmail.com>
2024-04-12 18:28:50 +0900

    delete: minHeightLayout

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -1,4 +1,3 @@
 export * from './metadata';
-export * from './minHeightLayout';
 export * from './nav';
 export * from './path';
--- src/utils/constants/minHeightLayout.ts
+++
@@ -1,3 +0,0 @@
-import { footerHeight, headerHeight } from '@/ui/layouts/reve';
-
-export const reveMinHeightLayout = `min-h-[calc(100vh-${headerHeight}px-${footerHeight}px)]`;
